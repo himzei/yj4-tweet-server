07f3281854faa73b4c3b73e6c731c46cb04577b4
조현일 <himzei@gmail.com>
2024-03-30 15:25:50 +0900

    re

--- src/index.js
+++ src/index.js
@@ -29,9 +29,10 @@ app.use(
     resave: false, // 세션이 변경되지 않아도 항상 저장되도록 설정합니다
     saveUninitialized: false, // 초기화되지 않은 세션을 저장소에 저장하지 않도록 설정합니다.
     cookie: {
+      httpOnly: true, // javascript에서 사용이 안되게 하는 옵션
+      secure: true, // HTTPS를 통해서만 세션 쿠키를 전송하도록 설정합니다.
+      sameSite: "none",
       maxAge: 1000 * 60 * 60 * 24,
-      httpOnly: false, // javascript에서 사용이 안되게 하는 옵션
-      secure: false, // HTTPS를 통해서만 세션 쿠키를 전송하도록 설정합니다.
     },
     store: MongoStore.create({ mongoUrl: process.env.DB_URL + "/yj4-twitter" }),
   })
